b3326e47f2a1cb11d3dece24393346412ebfee60
kwmorlock <kmorlockcodes@gmail.com>
2021-02-16 11:23:46 -0500

    set: usersModel.js exports

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,1 +1,35 @@
-//setup
+const db = require("../database/dbconfig");
+
+module.exports = {
+  add,
+  findUser,
+  findBy,
+  findById,
+  remove,
+};
+
+function findUser() {
+  return db("users").select("id", "username", "password", "email");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  try {
+    const [id] = await db("users").insert(user, "id");
+
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+function findById(id) {
+  return db("users").where({ id }).first();
+}
+
+function remove(id) {
+  return db("users").where("id", id).del();
+}
