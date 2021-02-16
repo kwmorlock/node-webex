70c766d2d86e871b2a909c005567d9ff79ef79bf
kwmorlock <kmorlockcodes@gmail.com>
2021-02-16 11:35:18 -0500

    set: authMiddleware.js jwt

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,1 +1,26 @@
-//setup
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "noneedforsecrets,tell the truth!";
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({
+          message:
+            "No entry allowed with this method, please take this time to do something nice for someone!",
+        });
+      } else {
+        req.jwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide the authentication information" });
+  }
+};
