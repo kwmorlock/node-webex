cf6b5d7219d366386e6bf65b1db03e3d778ba688
kwmorlock <kmorlockcodes@gmail.com>
2021-02-15 19:54:55 -0500

    set: server.js use get

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-//setup
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ api: "Ready To try Webex!!!" });
+});
+
+module.exports = server;
