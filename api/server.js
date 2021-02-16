cd10b1c615e1a0b45a0c9e7dc5d05d0882f01c8c
kwmorlock <kmorlockcodes@gmail.com>
2021-02-16 11:45:39 -0500

    set: server.js user paths

--- api/server.js
+++ api/server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const authenticate = require("../auth/authMiddleware");
+
+const usersAuthRouter = require("../auth/authUsersRouter");
+const usersRouter = require("../users/users/usersRouter");
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use("/api/users/users", authenticate, usersRouter);
+server.use("/api/users/auth", usersAuthRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "Ready To try Webex!!!" });
