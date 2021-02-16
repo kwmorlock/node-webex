5c83bc633b9468f725516932a3ee850e0a125001
kwmorlock <kmorlockcodes@gmail.com>
2021-02-16 11:43:26 -0500

    set: authUsersRouter.js posts

--- auth/authUsersRouter.js
+++ auth/authUsersRouter.js
@@ -1,1 +1,73 @@
-//setup
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+const Users = require("../users/usersModel.js");
+const { isValid } = require("../users/usersService.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide your username and password",
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ "users.username": username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = createToken(user);
+
+          res.status(200).json({ message: `${token}`, usersId: `${user.id}` });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide your username and password",
+    });
+  }
+});
+
+function createToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const secret = process.env.JWT_SECRET || "noneedforsecrets,tell the truth!";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
